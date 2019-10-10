d14cebc6d06377974e2084448fddd33dda9bef18
Julia <ggfox@mail.ru>
2019-10-10 16:34:48 +0300

    Доработка к 12-ому заданию

--- js/backend.js
+++ js/backend.js
@@ -1,69 +1,48 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-var form = userDialog.querySelector('.setup-wizard-form');
 
-window.save = function (formData, onLoad, onError) {
-  var URL = 'https://js.dump.academy/code-and-magick';
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    if (xhr.status === 200) {
-      onLoad(xhr.response);
-    } else {
-      onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-    }
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
+(function () {
+  var TIME_OUT = 10000;
+  var CASE_200 = 200;
+  var userDialog = document.querySelector('.setup');
+  var form = userDialog.querySelector('.setup-wizard-form');
+
+  window.save = function (url, method, formData, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === CASE_200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+      xhr.timeout = TIME_OUT;
     });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-    xhr.timeout = 10000; // 10s
+    xhr.open(method, url);
+    xhr.send(formData);
+  };
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.save('https://js.dump.academy/code-and-magick', 'POST', new FormData(form), function () {
+      userDialog.classList.add('hidden');
+    }, window.errorHandler);
   });
 
-  xhr.open('POST', URL);
-  xhr.send(formData);
-};
+  window.errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: grey;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '40px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
 
-form.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  window.save(new FormData(form), function () {
-    userDialog.classList.add('hidden');
-  }, window.errorHandler);
-});
-
-window.load = function (onLoad, onError) {
-  var URL = 'https://js.dump.academy/code-and-magick/data';
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    if (xhr.status === 200) {
-      onLoad(xhr.response);
-    } else {
-      onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-    }
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-    xhr.timeout = 10000; // 10s
-  });
-
-  xhr.open('GET', URL);
-  xhr.send();
-};
-
-window.errorHandler = function (errorMessage) {
-  var node = document.createElement('div');
-  node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: grey;';
-  node.style.position = 'absolute';
-  node.style.left = 0;
-  node.style.right = 0;
-  node.style.fontSize = '40px';
-  node.textContent = errorMessage;
-  document.body.insertAdjacentElement('afterbegin', node);
-};
-
+})();
--- js/setup.js
+++ js/setup.js
@@ -8,62 +8,6 @@
     .content.querySelector('.setup-similar-item');
   document.querySelector('.setup-similar').classList.remove('hidden');
 
-  // var playerNames = [
-  //   'Иван',
-  //   'Хуан',
-  //   'Себастьян',
-  //   'Мария',
-  //   'Кристоф',
-  //   'Виктор',
-  //   'Юлия',
-  //   'Люпита',
-  //   'Вашингтон'
-  // ];
-  // var surNames = [
-  //   'да Марья',
-  //   'Верон',
-  //   'Мирабелла',
-  //   'Вальц',
-  //   'Онопко',
-  //   'Топольницкая',
-  //   'Нионго',
-  //   'Ирвинг'
-  // ];
-  // var colors = [
-  //   'rgb(101, 137, 164)',
-  //   'rgb(241, 43, 107)',
-  //   'rgb(146, 100, 161)',
-  //   'rgb(56, 159, 117)',
-  //   'rgb(215, 210, 55)',
-  //   'rgb(0, 0, 0)'
-  // ];
-  // var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-
-  // window.random = function (min, max) {
-  //   if (max === undefined) {
-  //     max = min;
-  //     min = 0;
-  //   }
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // };
-
-  // var generatePlayers = function (arrayName, arraySur, arrayColor, arrayEyes) {
-  //   var players = [];
-
-  //   for (var i = 0; i <= 3; i++) {
-  //     var namerand = window.random(arrayName.length - 1);
-  //     var surand = window.random(arraySur.length - 1);
-  //     var colorrand = window.random(arrayColor.length - 1);
-  //     var eyesrand = window.random(arrayEyes.length - 1);
-  //     var gamePlayer = {
-  //       name: arrayName[namerand] + ' ' + arraySur[surand],
-  //       coatColor: arrayColor[colorrand],
-  //       eyesColor: arrayEyes[eyesrand]
-  //     };
-  //     players.push(gamePlayer);
-  //   }
-  //   return players;
-  // };
   var renderWizard = function (player) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = player.name;
@@ -72,8 +16,6 @@
 
     return wizardElement;
   };
-
-  // var igroki = generatePlayers(playerNames, surNames, colors, eyesColor);
 
 
   window.successHandler = function (wizards) {
@@ -87,7 +29,7 @@
     userDialog.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  window.load(window.successHandler, window.errorHandler);
+  window.save('https://js.dump.academy/code-and-magick/data', 'GET', '', window.successHandler, window.errorHandler);
 
 })();
 
